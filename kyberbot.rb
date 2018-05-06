2b50be579ca4aa8dc4a6a6e8cc9ae49e020cfe41
Vinh Nguyen <nguyenducvinh92@gmail.com>
2018-05-06 16:36:51 +0700

    Change date when get result

--- kyberbot.rb
+++ kyberbot.rb
@@ -2,17 +2,21 @@ require 'telegram/bot'
 require 'daybreak'
 require 'pry'
 require 'dotenv/load'
+require 'date'
 
 class Kyberbot
   def main
     token = ENV["SECRET_TOKEN"]
 
     Telegram::Bot::Client.run(token) do |bot|
+      db = Daybreak::DB.new "kybergame.db"
+      db[:tests] ||= []
+      db[:correct_users] ||= []
+      db[:winners] ||= []
+      today = Time.now.strftime("%d/%m/%Y")
+      yesterday = (Date.today - 1).strftime("%d/%m/%Y")
+
       bot.listen do |message|
-        db = Daybreak::DB.new "kybergame.db"
-        db[:tests] ||= []
-        db[:correct_users] ||= []
-        today = Time.now.strftime("%d/%m/%Y")
         chat_id = message.chat.id
         message_text = message.text
 
@@ -36,7 +40,7 @@ class Kyberbot
                 break
               end
             end
-            write_data db, message, chat_id, mark, today
+            write_test_result db, message, chat_id, mark, today
             bot.api.send_message(chat_id: message.chat.id, text: "Your marks: #{mark}")
           else
             bot.api.send_message(chat_id: message.chat.id, text: "Ban da tra loi 1 lan roi, hay doi den ngay mai.")
@@ -44,21 +48,31 @@ class Kyberbot
         when message_text.include?('/result')
           password = message_text.split.last
           if password == ENV["TELEBOTPW"]
-            correct_users = db[:correct_users].detect do |correct_users_by_day|
-               correct_users_by_day[today]
+            winners_yesterday = db[:winners].detect do |winners_by_day|
+               winners_by_day[yesterday]
             end
-            text = if correct_users
-              correct_users[today].shuffle[0..2].join(", ")
+            if winners_yesterday
+              text = winners_yesterday[yesterday].join(", ")
             else
-              "Khong co ai tra loi dung."
+              correct_users = db[:correct_users].detect do |correct_users_by_day|
+                 correct_users_by_day[yesterday]
+              end
+              if correct_users
+                winners = correct_users[yesterday].shuffle[0..2]
+                text = winners.join(", ")
+                db[:winners] << {yesterday => winners}
+                db.set! :winners, db[:winners]
+              else
+                text = "Khong co ai tra loi dung."
+              end
             end
           else
             text = "Sai mat khau"
           end
           bot.api.send_message(chat_id: message.chat.id, text: text)
         end
-        db.close
       end
+      db.close
     end
   end
 
@@ -74,7 +88,7 @@ class Kyberbot
     return true
   end
 
-  def write_data db, message, chat_id, mark, today
+  def write_test_result db, message, chat_id, mark, today
     # write all of tests
     today_tests = db[:tests].detect do |test_by_date|
       test_by_date[today]
