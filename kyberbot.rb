6b7a322472c1f049bc4ff4df710cbff43f665dd3
Vinh Nguyen <nguyenducvinh92@gmail.com>
2018-05-08 02:33:57 +0700

    Edit database structure and refactoring logic code

--- kyberbot.rb
+++ kyberbot.rb
@@ -10,66 +10,31 @@ class Kyberbot
 
     Telegram::Bot::Client.run(token) do |bot|
       db = Daybreak::DB.new "kybergame.db"
-      db[:tests] ||= []
-      db[:correct_users] ||= []
-      db[:winners] ||= []
-      today = Time.now.strftime("%d/%m/%Y")
-      yesterday = (Date.today - 1).strftime("%d/%m/%Y")
+      question_ids = db[:questions].keys
 
       bot.listen do |message|
+        today = Time.now.strftime("%d/%m/%Y")
+        yesterday = (Date.today - 1).strftime("%d/%m/%Y")
+        db.load
+        db[:tests] ||= {}
+        db[:tests][today] ||= {}
+        db[:correct_users] ||= {}
+        db[:correct_users][today] ||= []
+        db[:winners] ||= {}
+        db[:winners][today] ||= []
         chat_id = message.chat.id
         message_text = message.text
 
         case
         when message_text == '/start'
-          bot.api.send_message(chat_id: chat_id, text: "Welcome to KyberNetwork!")
+          bot.api.send_message(chat_id: chat_id, text: "Welcome to KyberNetwork!",
+            reply_markup: Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: ["/test"], one_time_keyboard: true))
         when message_text == '/test'
-          if check_number_test(db, message.chat.id, today)
-            mark = 0
-            db[:questions].shuffle.each do |question|
-              question_text = question[:question]
-              answers  = Telegram::Bot::Types::ReplyKeyboardMarkup
-                .new(keyboard: [question[:answer1], question[:answer2]], one_time_keyboard: true)
-
-              bot.api.send_message(chat_id: chat_id, text: question_text, reply_markup: answers)
-
-              bot.listen do |answer|
-                if answer.text == question[:ra]
-                  mark += 1
-                end
-                break
-              end
-            end
-            write_test_result db, message, chat_id, mark, today
-            bot.api.send_message(chat_id: message.chat.id, text: "Your marks: #{mark}")
-          else
-            bot.api.send_message(chat_id: message.chat.id, text: "Ban da tra loi 1 lan roi, hay doi den ngay mai.")
-          end
-        when message_text.include?('/result')
-          password = message_text.split.last
-          if password == ENV["TELEBOTPW"]
-            winners_yesterday = db[:winners].detect do |winners_by_day|
-               winners_by_day[yesterday]
-            end
-            if winners_yesterday
-              text = winners_yesterday[yesterday].join(", ")
-            else
-              correct_users = db[:correct_users].detect do |correct_users_by_day|
-                 correct_users_by_day[yesterday]
-              end
-              if correct_users
-                winners = correct_users[yesterday].shuffle[0..2]
-                text = winners.join(", ")
-                db[:winners] << {yesterday => winners}
-                db.set! :winners, db[:winners]
-              else
-                text = "Khong co ai tra loi dung."
-              end
-            end
-          else
-            text = "Sai mat khau"
-          end
-          bot.api.send_message(chat_id: message.chat.id, text: text)
+          start_testing bot, db, chat_id, today
+        when message_text.split[0] == '/result'
+          get_result bot, db, message_text, chat_id, yesterday
+        else
+          handle_user_test bot, db, message, chat_id, question_ids, today
         end
       end
       db.close
@@ -77,41 +42,81 @@ class Kyberbot
   end
 
   private
-  def check_number_test db, chat_id, today
-    db[:tests].each do |test|
-      if user_tests = test[today]
-        return false if user_tests.detect do |user_test|
-          user_test[chat_id]
-        end
-      end
-    end
-    return true
+  def check_number_test db, chat_id, date
+    db[:tests][date][chat_id].nil? || db[:tests][date][chat_id][:status] != "tested"
   end
 
-  def write_test_result db, message, chat_id, mark, today
-    # write all of tests
-    today_tests = db[:tests].detect do |test_by_date|
-      test_by_date[today]
-    end
-    if today_tests
-      today_tests << {chat_id => mark}
+  def start_testing bot, db, chat_id, today
+    if check_number_test(db, chat_id, today)
+      db[:tests][today].merge!({chat_id => {:status => 'testing', :question_ids => [], :mark => 0}})
+      db.set! :tests, db[:tests]
+      send_question bot, db, chat_id, today
     else
-      db[:tests] << {today => [{chat_id => mark}]}
+      bot.api.send_message(chat_id: chat_id, text: "Ban da tra loi 1 lan roi, hay doi den ngay mai.")
     end
-    db.set! :tests, db[:tests]
+  end
 
-    #write users who answer correct all of questions
-    if mark == 3
-      correct_users = db[:correct_users].detect do |correct_by_date|
-        correct_by_date[today]
-      end
-      if correct_users
-        correct_users[today] << "@#{message.chat.username}"
+  def send_question bot, db, chat_id, date
+    question_ids = db[:questions].keys
+    question_id = (question_ids - db[:tests][date][chat_id][:question_ids]).sample
+
+    question = db[:questions][question_id]
+    question_text = question[:question]
+    answers = Telegram::Bot::Types::ReplyKeyboardMarkup
+      .new(keyboard: [question[:answer1], question[:answer2]], one_time_keyboard: true)
+
+    bot.api.send_message(chat_id: chat_id, text: question_text, reply_markup: answers)
+
+    # update question_ids of user test
+    db[:tests][date][chat_id][:question_ids] << question_id
+    db.set! :tests, db[:tests]
+  end
+
+  #write users who answer correct all of questions
+  def write_correct_user db, message, date
+     db[:correct_users][date] << "@#{message.chat.username}"
+     db.set! :correct_users, db[:correct_users]
+  end
+
+  def handle_user_test bot, db, message, chat_id, question_ids, today
+    user_test = db[:tests][today][chat_id]
+    if user_test && user_test[:status] ==  'testing'
+      mark = user_test[:mark]
+      current_question_id = user_test[:question_ids].last
+      mark += 1 if message.text == db[:questions][current_question_id][:ra]
+      user_test[:mark] = mark
+
+      # update status to "tested" if user has completed all of questions
+      if question_ids - user_test[:question_ids] == []
+        user_test[:status] = "tested"
+        write_correct_user(db, message, today) if mark == 3
+        bot.api.send_message(chat_id: message.chat.id, text: "Your mark: #{mark}")
       else
-        db[:correct_users] << {today => ["@#{message.chat.username}"]}
+        send_question bot, db, chat_id, today
       end
-      db.set! :correct_users, db[:correct_users]
+
+      db.set! :tests, db[:tests]
     end
+  end
+
+  def get_result bot, db, message_text, chat_id, date
+    password = message_text.split.last
+    text = if password == ENV["TELEBOTPW"]
+      if winners_yesterday = db[:winners][date]
+        "Nguoi chien thang trong ngay #{date}: #{winners_yesterday.join(', ')}"
+      elsif date_correct_users = db[:correct_users][date]
+        winners = date_correct_users.shuffle[0..2]
+        db[:winners][date] = winners
+        db.set! :winners, db[:winners]
+        winners.join(", ")
+        "Nguoi chien thang trong ngay #{date}: #{winners.join(', ')}"
+      else
+        "Khong co ai tra loi dung trong ngay #{date}."
+      end
+    else
+      "Sai mat khau"
+    end
+    bot.api.send_message(chat_id: chat_id, text: text)
   end
 end
 
